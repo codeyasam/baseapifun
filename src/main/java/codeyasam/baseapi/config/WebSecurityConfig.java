a6ba2b472b1be2b6f61193009a25b305258a2ded
Yasa Emmanuel <codeyasam@gmail.com>
2017-06-01 01:43:34 +0800

    Adds own WebSecurityConfig which configures authentication using userDetailsService and passwordEncoder

--- src/main/java/codeyasam/baseapi/config/WebSecurityConfig.java
+++ src/main/java/codeyasam/baseapi/config/WebSecurityConfig.java
@@ -1,9 +1,29 @@
 package codeyasam.baseapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled=true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	private UserDetailsService userDetailsService;
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public WebSecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder passwordEncoder) {
+		this.userDetailsService = userDetailsService;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@Autowired
+	public void configAuth(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(passwordEncoder);
+	}
 }
