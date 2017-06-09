aa0cab406d12f362428231afcdafe0a352a75651
Yasa Emmanuel <codeyasam@gmail.com>
2017-06-10 07:45:06 +0800

    adds a prompt to get storage application properties value

--- src/main/java/codeyasam/baseapi/BaseAPI.java
+++ src/main/java/codeyasam/baseapi/BaseAPI.java
@@ -1,16 +1,36 @@
 package codeyasam.baseapi;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import codeyasam.baseapi.config.StorageProperties;
 
 @SpringBootApplication
 public class BaseAPI {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(BaseAPI.class, args);
+		ApplicationContext ctx = SpringApplication.run(BaseAPI.class, args);
+		Path path = Paths.get("/home/codeyasam/Pictures/task.png");
+		Path textFile = Paths.get("/home/codeyasam/Desktop/encodedImage.txt");
+		try {
+			byte[] byteArr = Files.readAllBytes(path);
+			Files.write(textFile, Base64.getEncoder().encode(byteArr));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		StorageProperties storageProperties = (StorageProperties) ctx.getBean("storageProperties");
+		System.out.println(storageProperties.toString());
 	}
 	
 	@Bean
